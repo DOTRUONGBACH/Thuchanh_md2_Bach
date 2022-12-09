5269af73eedf43dddc919e7eae8df3cb61b6aa78
DOTRUONGBACH <vuabongdapro@gmail.com>
2022-12-09 11:07:06 +0700

    OK

---
+++ src/MenuSort.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MenuSort {
+    public static void menusort() {
+        Scanner scanner = new Scanner(System.in);
+        ManageStudent Manager = new ManageStudent();
+        ReadandWrite readandWrite = new ReadandWrite();
+        while (true) {
+            System.out.println("1. Sắp xếp tăng dần");
+            System.out.println("2. Sắp xếp giảm dần");
+            System.out.println("3. Back");
+            int Choice = ErrorStudent.creatErrint(scanner);
+            switch (Choice) {
+                case 1 -> Manager.sortSmalltobig();
+                case 2 -> Manager.sortBigtosmall();
+                case 3 -> Menu.Menu();
+            }
+        }
+    }
+}
